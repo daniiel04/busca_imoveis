d976482c7017dccf2f41147edc1984897058a76a
daniiel04 <151573063+daniiel04@users.noreply.github.com>
2024-06-25 17:36:42 +0100

    Update server.py

--- server.py
+++ server.py
@@ -137,7 +137,7 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
 
 
 # Criar um servidor HTTP com o manipulador personalizado
-with socketserver.TCPServer(("", 8000), CustomHandler) as httpd:
+with socketserver.TCPServer(("localhost", 8000), CustomHandler) as httpd:
     print("Servindo na porta", 8000)
     httpd.serve_forever()
 
